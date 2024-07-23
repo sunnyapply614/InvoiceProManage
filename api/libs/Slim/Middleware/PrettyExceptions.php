772b87d2d0785d3c1e37d9563a139cff761e1401
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-07-23 03:52:52 -0700

    Update PrettyExceptions.php

--- api/libs/Slim/Middleware/PrettyExceptions.php
+++ api/libs/Slim/Middleware/PrettyExceptions.php
@@ -1,58 +1,11 @@
 <?php
-/**
- * Slim - a micro PHP 5 framework
- *
- * @author      Josh Lockhart <info@slimframework.com>
- * @copyright   2011 Josh Lockhart
- * @link        http://www.slimframework.com
- * @license     http://www.slimframework.com/license
- * @version     2.3.5
- * @package     Slim
- *
- * MIT LICENSE
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 namespace Slim\Middleware;
 
-/**
- * Pretty Exceptions
- *
- * This middleware catches any Exception thrown by the surrounded
- * application and displays a developer-friendly diagnostic screen.
- *
- * @package Slim
- * @author  Josh Lockhart
- * @since   1.0.0
- */
 class PrettyExceptions extends \Slim\Middleware
 {
-    /**
-     * @var array
-     */
+ 
     protected $settings;
 
-    /**
-     * Constructor
-     * @param array $settings
-     */
     public function __construct($settings = array())
     {
         $this->settings = $settings;
@@ -76,12 +29,6 @@ class PrettyExceptions extends \Slim\Middleware
         }
     }
 
-    /**
-     * Render response body
-     * @param  array      $env
-     * @param  \Exception $exception
-     * @return string
-     */
     protected function renderBody(&$env, $exception)
     {
         $title = 'Slim Application Error';
